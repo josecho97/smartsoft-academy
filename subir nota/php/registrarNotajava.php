0acc1ed37a571a0f831ed66b35d856f2c20bd092
gilmar bautista <guilmarbau.m777@gmail.com>
2021-12-06 13:16:21 -0400

    subir nota

---
+++ subir nota/php/registrarNotajava.php
@@ -0,0 +1,32 @@
+
+<?php
+    include("conexion.php");
+	include("sesionIniciada.php");
+	
+
+    $nota = $_REQUEST['nota'];
+	
+	if($nota > 50){
+		$C = 'Java';
+		$ID = $nomUsuario.'-'.$C;
+		
+		$sql= "UPDATE clase SET nota='$nota', estado='completo' WHERE idClase='$ID'";
+	
+		if(mysqli_query($conexion,$sql)){
+			header("location:./misCursos.html");
+		}else{
+			//header("location:../informacionCursoHtml.html");
+		}
+	}else{
+		$C = 'Java';
+		$ID = $nomUsuario.'-'.$C;
+		$sql= "DELETE FROM clase WHERE idClase='$ID'";
+		if(mysqli_query($conexion,$sql)){
+			//header("location:../cursos/moduloHtml.html");
+			header("location:../misCursos.html");
+		}else{
+			header("location:../informacionCursoHtml.html");
+		}
+	}
+	mysqli_close($conexion);
+?>
